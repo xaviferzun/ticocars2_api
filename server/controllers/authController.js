e50f0828263e0fe3b857ca73d2caf214ba299359
xaviferzun <xaviferzun@gmail.com>
2026-03-03 14:32:57 -0600

    feature/KAN-17 Implement POST /api/auth/login with credential validation and JWT

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,12 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 //Here I create the POST /api/auth/register endpoint to register a new user in TicoCars
 
 const registerUser = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const {username, email, password} = req.body;
 
     //Type validation to avoid missing fields
     if (!username || !email || !password) {
@@ -16,7 +17,7 @@ const registerUser = async (req, res) => {
 
 
     //Verify if email exists
-    const existsUser = await User.findOne({ email });
+    const existsUser = await User.findOne({email});
 
     if (existsUser) {
       return res.status(400).json({
@@ -56,6 +57,63 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+//Here I create the POST /api/auth/login endpoint to login a user in TicoCars and get a JWT token
+
+const loginUser = async (req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    //Same type validation to avoid missing fields
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email y contraseña son requeridos.",
+      });
+    }
+
+
+    //Verify if user exists
+    const existUser = await User.findOne({email});
+
+    if (!existUser) {
+      return res.status(401).json({
+        message: "Credenciales inválidas.",
+      });
+    }
+
+    //Compare the password with the hash on database
+    const correctPassword = await bcrypt.compare(password, existUser.password);
+    if (!correctPassword) {
+      return res.status(401).json({
+        message: "Credenciales inválidas.",
+      });
+    }
+
+    //Here I create the JWT token with the user id 
+    //The token expire in 1 hour
+    const token = jwt.sign(
+      {id: existUser._id},
+      process.env.JWT_SECRET,
+      {expiresIn: "1h"}
+    );
+
+    //Send the token on response
+    res.status(200).json({
+      message: "Login exitoso. Bienvenido a TicoCars.",
+      token,
+    });
+
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error del servidor.",
+    });
+  }
+};
+
+
 module.exports = {
   registerUser,
+  loginUser,
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser} = require("../controllers/authController");
+const {registerUser, loginUser} = require("../controllers/authController");
 
 //Here I define the route for the register POST endpoint. Call the register function from authController
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
