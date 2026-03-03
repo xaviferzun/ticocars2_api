0112200a53d4de2a397c3144e3d86a39cdd947be
xaviferzun <xaviferzun@gmail.com>
2026-03-03 15:02:48 -0600

    KAN-18 Implement authentication middleware with JWT verification and req.user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {registerUser, loginUser} = require("../controllers/authController");
+const authenticate = require("../middlewares/authMiddleware");
 
 //Here I define the route for the register POST endpoint. Call the register function from authController
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", authenticate, (req, res) => {
+  res.json({message: "Access granted", user: req.user});
+});
 
 module.exports = router;
