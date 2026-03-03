e8ca8de8556e231a451a0daab4d4f6ac3d002b84
xaviferzun <xaviferzun@gmail.com>
2026-03-03 00:16:20 -0600

    feature/KAN-14 Configure MongoDB local connection with mongoose

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
+//Here I use dotenv to load environment variables, import the app and DB connection and start the server after connecting to DB
+require("dotenv").config();
 const app = require("./app");
-const PORT = 3000;
+const connection = require("./config/database");
+const PORT = process.env.PORT || 3000;
 
-//Inicio el servidor
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+//Function to connect the DB and start the server
+const start = async () => {
+  await connection();
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+start();
