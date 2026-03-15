03e92ac3f859854a70acd5b291594d1ab069db73
xaviferzun <xaviferzun@gmail.com>
2026-03-15 17:14:24 -0600

    feature/KAN-25 implement GET /api/vehicles/:id endpoint

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -10,11 +10,31 @@ router.post("/", async (req, res) => {
     res.status(201).json(savedVehicle);
   } catch (error) {
     res.status(500).json({
-      message: "Error creating vehicle",
+      message: "Error al crear el vehículo",
       error: error.message
     });
   }
 });
+
+//Here I define the route to get a vehicle by its ID
+router.get("/:id", async (req, res) => {
+  try {
+    const vehicle = await Vehicle.findById(req.params.id);
+    if (!vehicle) {
+      return res.status(404).json({
+        message: "Vehículo no encontrado"
+      });
+    }
+    res.json(vehicle);
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al recuperar el vehículo",
+      error: error.message
+    });
+  }
+});
+
 
 //Define the route for the GET endpoint to get all vehicles.
 module.exports = router;
