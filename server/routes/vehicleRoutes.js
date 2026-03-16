1715745e7d64b01723660c29a1f2e35b5539ad37
xaviferzun <xaviferzun@gmail.com>
2026-03-15 18:38:00 -0600

    feature/KAN-26 implement PUT /api/vehicles/:id endpoint

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -8,10 +8,11 @@ router.post("/", async (req, res) => {
     const vehicle = new Vehicle(req.body);
     const savedVehicle = await vehicle.save();
     res.status(201).json(savedVehicle);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error al crear el vehículo",
-      error: error.message
+  } 
+    catch (error) {
+      res.status(500).json({
+        message: "Error al crear el vehículo",
+        error: error.message
     });
   }
 });
@@ -26,15 +27,40 @@ router.get("/:id", async (req, res) => {
       });
     }
     res.json(vehicle);
+  } 
+    catch (error) {
+      res.status(500).json({
+        message: "Error al recuperar el vehículo",
+        error: error.message
+    });
+  }
+});
 
-  } catch (error) {
-    res.status(500).json({
-      message: "Error al recuperar el vehículo",
-      error: error.message
+//Here I define the route to update a vehicle by its ID
+router.put("/:id", async (req, res) => {
+  try {
+    const updVehicle = await Vehicle.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true}
+    );
+    if (!updVehicle) {
+      return res.status(404).json({
+        message: "Vehículo no encontrado"
+      });
+    }
+    res.json(updVehicle);
+  }
+    catch (error) {
+      res.status(500).json({
+        message: "Error al actualizar el vehículo",
+        error: error.message
     });
   }
 });
 
 
-//Define the route for the GET endpoint to get all vehicles.
+
+
+//Export the vehicle routes
 module.exports = router;
