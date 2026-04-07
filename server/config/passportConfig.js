78cf70acfd4ca55766200a1a6b346b3b683e8bc1
edposxavier <xavier.fernandez@edposcr.com>
2026-04-07 12:23:36 -0600

    feature/KAN-58 add Google OAuth2 with passport, configure routes and callback

---
+++ server/config/passportConfig.js
@@ -0,0 +1,41 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+//Configure Google OAuth2 strategy with credentials from .env
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        //Check if user already exists with this Google account
+        let existingUser = await User.findOne({ email: profile.emails[0].value });
+
+        if (existingUser) {
+          //If user exists just return it
+          return done(null, existingUser);
+        }
+
+        //If user does not exist create a new with Google data
+        const newUser = await User.create({
+          username: profile.displayName,
+          email: profile.emails[0].value,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          authProvider: "google",
+          status: "active",
+        });
+
+        return done(null, newUser);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
